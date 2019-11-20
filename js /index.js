3574f61648c621fecf8e8e7302df4669f77b1b2a
ecl5pse <fantamas20@gmail.com>
2019-11-20 18:40:28 +0200

    add array

--- js /index.js
+++ js /index.js
@@ -1,3 +1,35 @@
-'use strick';
+'use strict';
 
-const  arr = [];
+function MyArray()
+{
+
+    this.length = 0;
+    for (let value of arguments) {
+
+        this.push(value);
+    }
+
+
+
+
+MyArray.prototype = {
+
+    pop() {
+             if (this.length > 0) {
+             const result = this[--this.length];
+            delete this[this.length];
+            return result;
+            }
+        },
+
+    push() {
+             for (let value of arguments) {
+
+                    this[this.length++] = value;
+             }
+
+             return this.length;
+        },
+
+
+    };
